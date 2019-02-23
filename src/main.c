51ec55dc577ba0f224972d836e84d3519e7adf29
Timothée Fivaz <timothee.fivaz@gmail.com>
2019-02-23 23:21:01 +0100

    Doxygen comment added and return value fixed with define

--- src/main.c
+++ src/main.c
@@ -3,10 +3,15 @@
  */
 
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
+/**
+ * @brief Game entry point
+ *
+ * @return EXIT_SUCCESS
+ */
 int main(void) {
   printf("Hello, world!\r\n");
 
-  return 0;
+  return EXIT_SUCCESS;
 }
