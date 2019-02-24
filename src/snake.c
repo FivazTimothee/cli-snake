c18fb80f0dbbdab5878fff4623c00fd2372b85b3
Timothée Fivaz <timothee.fivaz@gmail.com>
2019-02-24 13:06:17 +0100

    Add a set of functions to move the snake in any direction

--- src/snake.c
+++ src/snake.c
@@ -15,6 +15,53 @@ static snake_part_t* new_empty_part(void) {
   return new_part;
 }
 
+/**
+ * @brief Helper to remove the tail of a snake, i.e. free its memory and empty its cell on the map
+ *
+ * @return A pointer on the snake head (or any part of the snake, actually)
+ */
+static void remove_tail(snake_part_t* snake_head) {
+  if (snake_head) {
+    snake_part_t* current_part = snake_head;
+
+    // Find the tail
+    while (current_part->prev_part) {
+      current_part = current_part->prev_part;
+    }
+
+    // Empty its cell, set new tail and free the memory
+    snake_part_t* new_tail = current_part->next_part;
+    map_set_cell(current_part->row_index, current_part->column_index, CELL_EMPTY);
+    new_tail->prev_part = NULL;
+    free(current_part);
+  }
+}
+
+/**
+ * @brief Helper to move a snake to a given cell. This cell MUST BE adjacent to the snake's head
+ *
+ * @param snake_head   A pointer on the head of the snake to move
+ * @param row         The index of the row where the new head goes
+ * @param column      The index of the column where the new head goes
+ *
+ * @return A pointer on the snake new head
+ */
+static snake_part_t* move_to(snake_part_t* snake_head, int row, int column) {
+  // Create new head
+  snake_part_t* new_head = new_empty_part();
+  snake_head->next_part = new_head;
+  new_head->prev_part = snake_head;
+  new_head->row_index = row;
+  new_head->column_index = column;
+  map_set_cell(new_head->row_index, new_head->column_index, CELL_SNAKE_HEAD);
+
+  // Change previous head to body
+  map_set_cell(snake_head->row_index, snake_head->column_index, CELL_SNAKE_BODY);
+
+  remove_tail(snake_head);
+  return new_head;
+}
+
 snake_part_t* snake_create(unsigned int size, unsigned int head_row, unsigned int head_col) {
   snake_part_t* head = new_empty_part();
   head->row_index = head_row;
@@ -55,5 +102,29 @@ void snake_place_on_map(snake_part_t* snake_head) {
       snake_part = snake_part->prev_part;
     }
   }
+}
+
+snake_part_t* snake_move_up(snake_part_t* snake_head) {
+  int new_row = ROW_INDEX_UP(snake_head->row_index);
+  int new_column = snake_head->column_index;
+  return move_to(snake_head, new_row, new_column);
+}
+
+snake_part_t* snake_move_down(snake_part_t* snake_head) {
+  int new_row = ROW_INDEX_DOWN(snake_head->row_index);
+  int new_column = snake_head->column_index;
+  return move_to(snake_head, new_row, new_column);
+}
+
+snake_part_t* snake_move_left(snake_part_t* snake_head) {
+  int new_row = snake_head->row_index;
+  int new_column = COLUMN_INDEX_LEFT(snake_head->column_index);
+  return move_to(snake_head, new_row, new_column);
+}
+
+snake_part_t* snake_move_right(snake_part_t* snake_head) {
+  int new_row = snake_head->row_index;
+  int new_column = COLUMN_INDEX_RIGHT(snake_head->column_index);
+  return move_to(snake_head, new_row, new_column);
 }
 
