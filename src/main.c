b5f528d37ef248c61820ef314b9a4a75be6be00b
Timothée Fivaz <timothee.fivaz@gmail.com>
2019-02-24 13:09:42 +0100

    Use the new functions to display a marvelous snake on the map

--- src/main.c
+++ src/main.c
@@ -15,8 +15,12 @@
 int main(void) {
   printf("Hello, world!\r\n");
 
+  snake_part_t* snake_head = snake_create(10, MAP_SIZE_V / 2, MAP_SIZE_H / 2);
   map_clear();
+  snake_place_on_map(snake_head);
   map_render();
+
+  snake_destroy(snake_head);
 
   return EXIT_SUCCESS;
 }
